5d406bb846eb091b51a8a164f86b36ddb3e163e3
SilviaAmAm <silvia@maykinmedia.nl>
2023-09-05 16:58:45 +0200

    :white_check_mark: [#23] Test visibility of config fields

--- src/open_zaaktypebeheer/api/config/tests/test_views.py
+++ src/open_zaaktypebeheer/api/config/tests/test_views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
+from open_zaaktypebeheer.accounts.tests.factories import UserFactory
+
 
 class ConfigurationViewTests(APITestCase):
     def test_configuration_view(self):
@@ -18,3 +20,27 @@ class ConfigurationViewTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertFalse(response.json()["oidcEnabled"])
+
+    def test_public_fields(self):
+        config_url = reverse("api:config:configuration")
+
+        with self.subTest("Not authenticated"):
+            response = self.client.get(config_url)
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+            data = response.json()
+
+            self.assertNotIn("openzaakAdminUrl", data)
+
+        user = UserFactory.create()
+        self.client.force_authenticate(user=user)
+
+        with self.subTest("Authenticated"):
+            response = self.client.get(config_url)
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+            data = response.json()
+
+            self.assertIn("openzaakAdminUrl", data)
