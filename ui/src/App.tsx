b2fa4537215b78660318766b307d74501d999e69
Sven van de Scheur <svenvandescheur@gmail.com>
2024-03-11 17:57:17 +0100

    :sparkles: - feat: implement detail view and improve application integration with @maykin-ui/admin-ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,84 +1,79 @@
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { AuthProvider, RequireAuth, RequireNoAuth, useAuth } from './components/Auth/Auth';
+import {RouterProvider, createBrowserRouter, Outlet} from 'react-router-dom';
+import {AuthProvider, RequireAuth} from './components/Auth/Auth';
 import BaseView from './views/BaseView';
 import LoginView from './views/LoginView';
 import DashboardView from './views/DashboardView/DashboardView';
 import ZaaktypeEditView from './views/ZaaktypeEditView';
-import { ThemeProvider } from '@emotion/react';
-import { theme } from './utils/theme';
+import {ThemeProvider} from '@emotion/react';
+import {theme} from './utils/theme';
 import ReportComplete from './components/Snackbar/Snackbar';
-import { SnackbarProvider } from 'notistack';
+import {SnackbarProvider} from 'notistack';
 import ZaaktypeView from './views/ZaaktypenView';
-import { ConfigProvider } from './components/Config/Config';
-import { A, Button, Navbar } from '@maykin-ui/admin-ui/components';
-import { NavigationContext } from '@maykin-ui/admin-ui/contexts';
-import { getSiteTree } from './utils/siteTree.tsx';
+import {ConfigProvider} from './components/Config/Config';
 
-/**
- * Create the navigation
- */
-function Nav() {
-  const auth = useAuth();
-  const navigate = useNavigate();
-  const siteTreeOptions = getSiteTree(navigate, auth);
 
-  return (
-    <Navbar>
-      {siteTreeOptions.map(({ href, Icon, label, onClick }) => {
-        return href ? (
-          <A key={label} href={href} onClick={onClick}>
-            {Icon}
-            {label}
-          </A>
-        ) : (
-          <Button key={label} onClick={onClick}>
-            {Icon}
-            {label}
-          </Button>
-        );
-      })}
-    </Navbar>
-  );
-}
+const router = createBrowserRouter([
+    {
+        element: <RequireAuth/>,
+        children: [
+            {
+                path: '/',
+                element: <BaseView/>,
+                children: [
+                    {
+                        path: '/zaaktypen/',
+                        element: <Outlet/>,
+                        handle: {
+                            labelTemplate: "Zaaktypen"
+                        },
+                        children: [
+                            {
+                                path: '',
+                                element: <DashboardView/>,
+                            },
+                            {
+                                path: ':zaaktypeUuid',
+                                element: <ZaaktypeView/>,
+                                handle: {
+                                    labelTemplate: "Zaaktype ({zaaktypeUuid})"
+                                }
+                            },
+                            {
+                                path: '/zaaktypen/:zaaktypeUuid/wijzigen',
+                                element: <ZaaktypeEditView/>,
+                                handle: {
+                                    labelTemplate: "Zaaktype bewerken ({zaaktypeUuid})"
+                                }
+                            },
+                        ]
+                    },
+                ]
+            }
+        ],
+    },
+    {path: '/login', element: <LoginView/>}
+])
+
 
 function App() {
-  return (
-    <ConfigProvider>
-      <ThemeProvider theme={theme}>
-        <SnackbarProvider
-          maxSnack={1}
-          Components={{
-            success: ReportComplete,
-            error: ReportComplete,
-            unsavedChanges: ReportComplete,
-          }}
-        >
-          <AuthProvider>
-            <NavigationContext.Provider value={{ primaryNavigation: <Nav /> }}>
-              <Routes>
-                <Route element={<RequireAuth />}>
-                  <Route path="/" element={<DashboardView />} />
-                  <Route path="/zaaktypen" element={<DashboardView />} />
-
-                  <Route path="/" element={<BaseView />}>
-                    <Route path="/zaaktypen/:zaaktypeUuid" element={<ZaaktypeView />} />
-                    <Route
-                      path="/zaaktypen/:zaaktypeUuid/wijzigen"
-                      element={<ZaaktypeEditView />}
-                    />
-                  </Route>
-                </Route>
-
-                <Route element={<RequireNoAuth />}>
-                  <Route path="/login" element={<LoginView />} />
-                </Route>
-              </Routes>
-            </NavigationContext.Provider>
-          </AuthProvider>
-        </SnackbarProvider>
-      </ThemeProvider>
-    </ConfigProvider>
-  );
+    return (
+        <ConfigProvider>
+            <ThemeProvider theme={theme}>
+                <SnackbarProvider
+                    maxSnack={1}
+                    Components={{
+                        success: ReportComplete,
+                        error: ReportComplete,
+                        unsavedChanges: ReportComplete,
+                    }}
+                >
+                    <AuthProvider>
+                        <RouterProvider router={router}></RouterProvider>
+                    </AuthProvider>
+                </SnackbarProvider>
+            </ThemeProvider>
+        </ConfigProvider>
+    );
 }
 
 export default App;
--- ui/src/views/LoginView.tsx
+++ ui/src/views/LoginView.tsx
@@ -1,65 +1,67 @@
-import React, { useState } from 'react';
-import { useAuth } from '../components/Auth/Auth';
-import { useNavigate } from 'react-router-dom';
-import { APIError, BadRequest } from '../errors/errors';
-import { useConfig } from '../components/Config/Config.tsx';
-import { Login } from '@maykin-ui/admin-ui/templates';
+import React, {useState} from 'react';
+import {useAuth} from '../components/Auth/Auth';
+import {useNavigate} from 'react-router-dom';
+import {APIError, BadRequest} from '../errors/errors';
+import {useConfig} from '../components/Config/Config.tsx';
+import {Login} from '@maykin-ui/admin-ui/templates';
 import '@maykin-ui/admin-ui/style';
 
 export default function LoginView() {
-  const [error, setError] = useState<APIError>(undefined!);
-  const config = useConfig();
-  const auth = useAuth();
-  const navigate = useNavigate();
+    const [error, setError] = useState<APIError>(undefined!);
+    const config = useConfig();
+    const auth = useAuth();
+    const navigate = useNavigate();
 
-  const getOidcLoginUrl = () => {
-    const nextUrl = new URL('/', window.location.href);
-    return config.oidcLoginUrl + '?next=' + nextUrl.href;
-  };
+    const getOidcLoginUrl = () => {
+        const nextUrl = new URL('/', window.location.href);
+        return config.oidcLoginUrl + '?next=' + nextUrl.href;
+    };
 
-  async function handleLogin(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    try {
-      await auth.onSignIn(data);
-      navigate('/', { replace: true });
-    } catch (err) {
-      setError(err as BadRequest);
+    async function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        try {
+            await auth.onSignIn(data);
+            navigate('/', {replace: true});
+        } catch (err) {
+            setError(err as BadRequest);
+        }
     }
-  }
 
-  return (
-    <Login
-      nonFieldErrors={error?.message}
-      fields={[
-        {
-          autoComplete: 'username',
-          autoFocus: true,
-          label: 'Gebruikersnaam',
-          name: 'username',
-          required: true,
-        },
-        {
-          autoComplete: 'current-password',
-          label: 'Wachtwoord',
-          name: 'password',
-          required: true,
-          type: 'password',
-        },
-      ]}
-      secondaryActions={
-        config.oidcEnabled
-          ? [
-              {
-                href: getOidcLoginUrl(),
-                children: 'Inloggen via organisatie',
-                size: 'xs',
-                variant: 'transparent',
-              },
-            ]
-          : undefined
-      }
-      onSubmit={handleLogin}
-    />
-  );
+    return (
+        <Login
+            formProps={{
+                nonFieldErrors: error?.message,
+                fields: [
+                    {
+                        autoComplete: 'username',
+                        autoFocus: true,
+                        label: 'Gebruikersnaam',
+                        name: 'username',
+                        required: true,
+                    },
+                    {
+                        autoComplete: 'current-password',
+                        label: 'Wachtwoord',
+                        name: 'password',
+                        required: true,
+                        type: 'password',
+                    },
+                ],
+                secondaryActions:
+                    config.oidcEnabled
+                        ? [
+                            {
+                                href: getOidcLoginUrl(),
+                                children: 'Inloggen via organisatie',
+                                size: 'xs',
+                                variant: 'transparent',
+                            },
+                        ]
+                        : undefined
+                ,
+                onSubmit: handleLogin
+            }}
+        />
+    );
 }
